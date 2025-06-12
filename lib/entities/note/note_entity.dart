cbc6757170871ba71b88e4ab0a9d6ebffa71d822
Brandon Guigo <brandon.guigo@gmail.com>
2025-06-12 17:23:40 +0200

    feat: add note entity
    Signed-off-by: Brandon Guigo <brandon.guigo@gmail.com>

---
+++ lib/entities/note/note_entity.dart
@@ -0,0 +1,58 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes/services/encryption.service.dart';
+
+part 'note_entity.freezed.dart';
+part 'note_entity.g.dart';
+
+@unfreezed
+class Note with _$Note {
+  Note._();
+  factory Note({
+    String? id,
+    String? title,
+    String? content,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) = _Note;
+
+  static final nonEncryptedFields = [
+    'id',
+    'createdAt',
+    'updatedAt',
+  ];
+
+  factory Note.fromJson(Map<String, dynamic> json) => _$NoteFromJson(json);
+
+  @override
+  String toString() {
+    return 'Note { id: $id, title: $title, content: $content, created_at: $createdAt, updated_at: $updatedAt }';
+  }
+
+  Future<Map<String, dynamic>> encrypt(
+      {required EncryptionService encryptionService}) async {
+    Map<String, dynamic> encryptedData = {
+      'id': id,
+      'title': await encryptionService.encryptJson(title),
+      'content': await encryptionService.encryptJson(content),
+      'createdAt': createdAt?.toUtc().toIso8601String(),
+      'updatedAt': updatedAt?.toUtc().toIso8601String(),
+    };
+    return encryptedData;
+  }
+
+  static Future<Note> decrypt(
+      Map<String, dynamic> data, EncryptionService encryptionService) async {
+    Map<String, dynamic> decryptedData = {};
+
+    for (var entry in data.entries) {
+      if (nonEncryptedFields.contains(entry.key)) {
+        decryptedData[entry.key] = entry.value;
+      } else {
+        decryptedData[entry.key] =
+            await encryptionService.decryptJson(entry.value);
+      }
+    }
+
+    return Note.fromJson(decryptedData);
+  }
+}
--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 1339 (669 per locale)
 ///
-/// Built on 2025-06-12 at 14:59 UTC
+/// Built on 2025-06-12 at 15:22 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
